3d9c324c0007fc86306771ac380ce59368f966e3
ybadr99 <yousefbadr909@gmail.com>
2023-07-12 12:48:28 +0300

    update quries on animals table

--- queries.sql
+++ queries.sql
@@ -13,3 +13,32 @@ SELECT * FROM animals WHERE neutered IS TRUE;
 SELECT * FROM animals WHERE name != 'Gabumon';
 
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+-- update animals
+-- 1
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species='';
+COMMIT;
+-- 2
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+-- 3
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-1-1';
+SAVEPOINT s1;
+UPDATE animals SET weight_kg = weight_kg*-1;
+ROLLBACK TO s1;
+UPDATE animals SET weight_kg = weight_kg*-1 WHERE weight_kg < 0;
+COMMIT;
+-- 4
+SELECT count(*) FROM animals;
+SELECT count(*) FROM animals WHERE escape_attempts >= 1;
+SELECT avg(weight_kg) FROM animals;
+SELECT max(escape_attempts) FROM animals;
+SELECT max(weight_kg),min(weight_kg),species FROM animals group by species;
+SELECT avg(escape_attempts),species FROM animals WHERE date_of_birth > '1999-1-1' and date_of_birth > '2000-1-1'  group by species;
