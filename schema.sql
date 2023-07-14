bb985aaed6d6f5872b89013518e8af4c9a058d41
ybadr99 <yousefbadr909@gmail.com>
2023-07-14 18:35:13 +0300

    update schema and data

--- schema.sql
+++ schema.sql
@@ -19,6 +19,27 @@ CREATE TABLE animals (
     species INT 
 )
 
+
 ALTER TABLE animals DROP column species;
 ALTER TABLE animals ADD species_id int REFERENCES species(id);
 ALTER TABLE animals ADD owner_id int REFERENCES owners(id);
+
+CREATE TABLE vets (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255) NOT NULL,
+    age INT NOT NULL,
+    date_of_graduation DATE
+);
+
+CREATE TABLE specializations (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    vets_id INT REFERENCES vets(id),
+    species_id INT REFERENCES species(id)
+);
+
+CREATE TABLE visits (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    vets_id INT REFERENCES vets(id),
+    animals_id INT REFERENCES animals(id),
+    date Date NOT NULL
+);
